f7170fa76209263bd362692b16f9c48dd4be26ee
John Kallis <johnkallis01@gmail.com>
2025-02-22 17:40:33 -0500

    s

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import authRoutes from './routes/auth.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 const connectDB = async () => {
     // console.log(process.env)
