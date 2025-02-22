1b6f89d1bbca96c3fe78bb76f474dfce17e74b3e
John Kallis <johnkallis01@gmail.com>
2025-02-22 17:43:30 -0500

    v

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://localhost:8080',
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': 'https://localhost:8080',
+  //   }
+  // }
 })
